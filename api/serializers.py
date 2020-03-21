14dbcf7e00a10527a6a284d2ef5e233b5c2ee8c9
Bruce Roettgers <bruce@roettgers.co>
2020-03-21 14:05:58 +0100

    eigenes TimeSlot model

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Anfrage
+from .models import Anfrage, TimeSlot
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,8 +15,39 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'name']
 
 
-class AnfrageSerializer(serializers.HyperlinkedModelSerializer):
+# class AnfrageSerializer(serializers.Serializer):
+#     unternehmen_id = serializers.IntegerField()
+#     kunden_email = serializers.EmailField()
+#     text = serializers.CharField()
+#     slot = serializers.DictField(child=serializers.DateTimeField)
+#
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `Anfrage` instance, given the validated data.
+#         """
+#         slot = TimeSlot.objects.create(start=validated_data['slot']['start_datum'], end=validated_data['slot']['end_datum'])
+#         return Anfrage.objects.create(
+#             unternehmen=User.objects.get(validated_data['unternehmen_id']),
+#             kunden_email=validated_data['kunden_email'],
+#             text=validated_data['text'],
+#             slot=slot
+#         )
+
+
+class SlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimeSlot
+        fields = '__all__'
+
+
+class AnfrageSerializer(serializers.ModelSerializer):
+    slot = SlotSerializer()
 
     class Meta:
         model = Anfrage
-        fields = ['unternehmen', 'kunden_email', 'text', 'start_datum', 'end_datum']
+        fields = '__all__'
+
+    def create(self, validated_data):
+        slot_data = validated_data.pop('slot')
+        slot = TimeSlot.objects.create(**slot_data)
+        return Anfrage.objects.create(slot=slot, **validated_data)
