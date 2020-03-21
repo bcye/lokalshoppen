dc3f0e4ba842905f2f7ad354d4ba916ebeca7ab0
Bruce Roettgers <bruce@roettgers.co>
2020-03-21 17:22:11 +0100

    change to postgres on dev

--- api/models.py
+++ api/models.py
@@ -8,10 +8,18 @@ class TimeSlot(models.Model):
 
 
 class Anfrage(models.Model):
-    unternehmen_idy = models.ForeignKey(User, on_delete=models.CASCADE)
+    unternehmen_id = models.ForeignKey(User, on_delete=models.CASCADE)
     kunden_email = models.EmailField()
     text = models.CharField(max_length=500)
     approved = models.BooleanField()
     slot = models.OneToOneField(TimeSlot, on_delete=models.CASCADE)
 
 
+class UnternehmenProfil(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    adresse = models.TextField()
+    langitude = models.TextField()
+    longitude = models.TextField()
+    telefon = models.TextField()
+    max_pro_slot = models.PositiveSmallIntegerField()
