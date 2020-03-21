2c7212468aef6a4022d65fe9e768431877c98b25
Bruce Roettgers <bruce@roettgers.co>
2020-03-21 14:31:01 +0100

    remove unnecessary fields

--- api/models.py
+++ api/models.py
@@ -11,7 +11,5 @@ class Anfrage(models.Model):
     unternehmen = models.ForeignKey(User, on_delete=models.CASCADE)
     kunden_email = models.EmailField()
     text = models.CharField(max_length=500)
-    start_datum = models.DateTimeField()
-    end_datum = models.DateTimeField()
     approved = models.BooleanField()
     slot = models.OneToOneField(TimeSlot, on_delete=models.CASCADE)
