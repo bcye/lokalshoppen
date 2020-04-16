9469c70b6ef02f336ec1220acd33ebd7dc172a64
Bruce Roettgers <bruce@roettgers.co>
2020-04-16 22:52:48 +0200

    .

--- lokalshoppen/settings.py
+++ lokalshoppen/settings.py
@@ -41,7 +41,7 @@ if DEBUG == False:
     )
 
 # DEPLOY
-SECURE_SSL_REDIRECT = not debug_val()
+# SECURE_SSL_REDIRECT = not debug_val()
 SESSION_COOKIE_SECURE = not debug_val()
 CSRF_COOKIE_SECURE = not debug_val()
 
