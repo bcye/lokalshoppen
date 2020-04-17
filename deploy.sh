339b120dc8864370ea99e6b09e2fe52585080434
Bruce Roettgers <bruce@roettgers.co>
2020-04-17 19:53:02 +0200

    oof

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 travis_rsa # Allow read access to the private key
 ssh-add travis_rsa # Add the private key to SSH
-ssh-keyscan -H dev.bleiblokal.com >> ~/.ssh/known_hosts
+ssh-keyscan -H api.bleiblokal.com >> ~/.ssh/known_hosts
 
-git remote add deploy dokku@dev.bleiblokal.com:lokalshoppen
+git remote add deploy dokku@api.bleiblokal.com:lokalshoppen
 git push deploy dev
 
 EOF
